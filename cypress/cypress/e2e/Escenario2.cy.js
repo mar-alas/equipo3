6049ca37eb81ac4adb01332620cb50fccfbfe771
DaGamez <da.gamez96@uniandes.edu.co>
2023-11-11 15:36:49 -0500

    ajuste cypress archivos iniciales

--- cypress/cypress/e2e/Escenario2.cy.js
+++ cypress/cypress/e2e/Escenario2.cy.js
@@ -1,4 +1,11 @@
 import loginPage from './authentication';
+//import properties
+
+//const config= require('./config.json');
+//import loginPage from './config.json';
+
+//const EMAIL= "da.gamez97@gmail.com";
+//const PASSWORD= "pPb8c@Jw0c4RyK1i";
 
 describe('Funcionalidad de autenticacion', () => {
   beforeEach(() => {
@@ -7,8 +14,8 @@ describe('Funcionalidad de autenticacion', () => {
 
   it('Login con exito', () => {
     // Given my email and my password
-    loginPage.fillEmail('user@example.com');
-    loginPage.fillPassword('dzLY3PdWLWj:');
+    loginPage.fillEmail("user@example.com");
+    loginPage.fillPassword("dzLY3PdWLWj:");
     
     // When I click in login
     loginPage.submitLoginForm();
