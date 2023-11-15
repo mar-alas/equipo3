4faf23045eb3951de2f1e2c47b34a83b9ea4a9c9
Jhon Fredddy Puentes <j.puentesn@uniandes.edu.co>
2023-11-15 23:21:57 +0400

    feat(screenshots): Toma de capturas para el escenario 1. Configuracion dinamica de ruta para las fotos en archivo de configuracion.

--- cypress/cypress/e2e/Escenario01.cy.js
+++ cypress/cypress/e2e/Escenario01.cy.js
@@ -1,7 +1,9 @@
 import loginPage from './authentication';
 import crearPost from './post';
 
-describe('Escenario 1', () => {
+const NOMBRE_ESCENARIO = 'Escenario 1 -- Paso ';
+
+describe(NOMBRE_ESCENARIO, () => {
   beforeEach(() => {
     loginPage.visit();
   });
@@ -9,19 +11,31 @@ describe('Escenario 1', () => {
   it('Login con exito, creacion y validacion de un post, signout', () => {
     // Given I log in in ghost
     loginPage.fillEmail(Cypress.env("username"));
+    cy.screenshot(NOMBRE_ESCENARIO + '1_fillEmail');
+
     loginPage.fillPassword(Cypress.env("password"));
+    cy.screenshot(NOMBRE_ESCENARIO + '2_fillPassword');
+
     loginPage.submitLoginForm();
+    cy.screenshot(NOMBRE_ESCENARIO + '3_submitLoginForm');
+
     cy.url().should('include', '/dashboard');
+    cy.screenshot(NOMBRE_ESCENARIO + '4_includeDashboard');
 
     //When I create a new post called "hola" 
 	  crearPost.crearPost("hola");
+    cy.screenshot(NOMBRE_ESCENARIO + '5_crearPost');
 
     //Then I should have the three post correcly
     crearPost.existePost("hola");
+    cy.screenshot(NOMBRE_ESCENARIO + '6_existePost');
 
     // When I signout
     loginPage.signout();
+    cy.screenshot(NOMBRE_ESCENARIO + '7_signout');
+    
     // Then I should be on the signin page
     cy.url().should('include', '/signin');
+    cy.screenshot(NOMBRE_ESCENARIO + '8_includeSignin');
   });
 });
