6d537ff3f70aa6133a4ba03762ef7ed8756b4d4a
Jhon Fredddy Puentes <j.puentesn@uniandes.edu.co>
2023-11-18 09:57:27 +0100

    feat(screenshot): Toma de capturas para escenario 17.

--- cypress/cypress/e2e/Escenario17.cy.js
+++ cypress/cypress/e2e/Escenario17.cy.js
@@ -2,38 +2,54 @@
 import loginPage from "./authentication";
 import crearPagina from "./pagina"; 
 
+const NOMBRE_ESCENARIO = 'Escenario 17 -- Paso ';
+
 describe("Crear pagina Ghost", () => {
   beforeEach(() => {
     loginPage.visit(); 
+    cy.screenshot(NOMBRE_ESCENARIO + '0_visit');
   });
 
   it("Login y creación de un page luego editarla", () => {
     loginPage.fillEmail(Cypress.env("username")); 
+    cy.screenshot(NOMBRE_ESCENARIO + '1_fillEmail');
+
     loginPage.fillPassword(Cypress.env("password"));
+    cy.screenshot(NOMBRE_ESCENARIO + '2_fillPassword');
+
     loginPage.submitLoginForm();
+    cy.screenshot(NOMBRE_ESCENARIO + '3_submitLoginForm');
+
     cy.url().should("include", "/dashboard");
+    cy.screenshot(NOMBRE_ESCENARIO + '4_shouldIncludeDashboard');
 
     crearPagina.navegarAPages();
     cy.wait(10000);
+    cy.screenshot(NOMBRE_ESCENARIO + '5_navegarAPages');
 
     //When I create a new page
 	  crearPagina.crearPagina("Mi pagina 1");
     cy.wait(10000);
+    cy.screenshot(NOMBRE_ESCENARIO + '6_crearPagina');
 
     //Then I should have the page correcly
     crearPagina.existePage("Mi pagina 1");
     cy.wait(10000);
+    cy.screenshot(NOMBRE_ESCENARIO + '7_existePage');
 
     crearPagina.editarPagina("Mi pagina 1", "Contenido nuevo 1");
     cy.wait(20000);
+    cy.screenshot(NOMBRE_ESCENARIO + '8_editarPagina');
 
     crearPagina.revisarContenidoPagina("Mi pagina 1", "Contenido nuevo 1");
-    
+    cy.screenshot(NOMBRE_ESCENARIO + '9_revisarContenidoPagina');
+
     // When I signout
     loginPage.signout();
+    cy.screenshot(NOMBRE_ESCENARIO + '10_signout');
 
     // Then I should be on the signin page
     cy.url().should('include', '/signin');
-    
+    cy.screenshot(NOMBRE_ESCENARIO + '11_shouldIncludeSignin');
   });
 });
